4be644d9484faead1b82832e7e875fbfb2d0d4cb
CreamySushi <johnaaronflavien@gmail.com>
2025-10-27 22:34:24 +0800

    divide commit

--- calculator.js
+++ calculator.js
@@ -7,8 +7,12 @@ function SUBTRACT(a,b){
 function MULTIPLY(a,b){
     return a*b
 }
+function DIVIDE(a,b){
+    return a/b
+}
 let a = 10
 let b = 12
 console.log("ADD: ", ADD(a,b))
 console.log("SUBTRACT: ", SUBTRACT(a,b))
 console.log("MULTIPLY: ", MULTIPLY(a,b))
+console.log("DIVIDE: ", DIVIDE(a,b))
