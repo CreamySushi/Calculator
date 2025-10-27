ad5da812067b9dc97e17e249194477689285bae2
CreamySushi <johnaaronflavien@gmail.com>
2025-10-27 22:32:08 +0800

    multiply commit

--- calculator.js
+++ calculator.js
@@ -4,8 +4,11 @@ function ADD(a,b){
 function SUBTRACT(a,b){
     return a-b
 }
-
+function MULTIPLY(a,b){
+    return a*b
+}
 let a = 10
 let b = 12
 console.log("ADD: ", ADD(a,b))
 console.log("SUBTRACT: ", SUBTRACT(a,b))
+console.log("MULTIPLY: ", MULTIPLY(a,b))
