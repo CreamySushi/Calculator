76ae61c14720858e593c3b614f71dccb84e63837
CreamySushi <johnaaronflavien@gmail.com>
2025-10-27 22:30:35 +0800

    subtract commit

--- calculator.js
+++ calculator.js
@@ -1,7 +1,11 @@
 function ADD(a,b){
     return a+b
 }
+function SUBTRACT(a,b){
+    return a-b
+}
 
 let a = 10
 let b = 12
 console.log("ADD: ", ADD(a,b))
+console.log("SUBTRACT: ", SUBTRACT(a,b))
