7bd9f35bd7927081bb68d65572fa4d8aefb39437
CreamySushi <johnaaronflavien@gmail.com>
2025-10-27 22:36:26 +0800

    square - feature/square branch

--- calculator.js
+++ calculator.js
@@ -10,9 +10,13 @@ function MULTIPLY(a,b){
 function DIVIDE(a,b){
     return a/b
 }
+function SQUARE(a,b){
+    return a**b
+}
 let a = 10
 let b = 12
 console.log("ADD: ", ADD(a,b))
 console.log("SUBTRACT: ", SUBTRACT(a,b))
 console.log("MULTIPLY: ", MULTIPLY(a,b))
 console.log("DIVIDE: ", DIVIDE(a,b))
+console.log("SQUARE: ", SQUARE(a,b))
