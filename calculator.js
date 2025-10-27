835410d9f3ec4095892fe82bebfc2694ecd4e698
CreamySushi <johnaaronflavien@gmail.com>
2025-10-27 22:39:01 +0800

    remove add commit

--- calculator.js
+++ calculator.js
@@ -1,6 +1,3 @@
-function ADD(a,b){
-    return a+b
-}
 function SUBTRACT(a,b){
     return a-b
 }
@@ -15,7 +12,6 @@ function SQUARE(a,b){
 }
 let a = 10
 let b = 12
-console.log("ADD: ", ADD(a,b))
 console.log("SUBTRACT: ", SUBTRACT(a,b))
 console.log("MULTIPLY: ", MULTIPLY(a,b))
 console.log("DIVIDE: ", DIVIDE(a,b))
